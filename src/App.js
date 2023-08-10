9e01a36491b985b9a4261ccf46f708ac70c81a9f
mergd <24722014+mergd@users.noreply.github.com>
2023-08-10 10:12:32 -0700

    Clean up the app a little

--- src/App.js
+++ src/App.js
@@ -1,44 +1,74 @@
 import React, { useState } from "react";
+import { FaCopy, FaPaste, FaTrash } from "react-icons/fa";
 import "./App.css";
 
 const App = () => {
   const [text, setText] = useState("");
+  const [showToast, setShowToast] = useState(false);
 
-  const handlePaste = async () => {
+  const handlePaste = (event) => {
     try {
-      const clipboardText = await navigator.clipboard.readText();
+      const clipboardText = event.clipboardData.getData("text");
       setText(removeCitations(clipboardText));
     } catch (err) {
       console.error("Failed to read clipboard contents: ", err);
     }
   };
 
-  const removeCitations = (text) => {
-    // remove bracketed citations
-    let newText = text.replace(/\[\d+\]/g, "");
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 2000);
+  };
 
-    // remove "Citations:" section
+  const handleClear = () => {
+    setText("");
+  };
+
+  const removeCitations = (text) => {
+    // remove bracketed citations outside of code blocks
+    let newText = text.replace(/(?<!`)((?<!\w)\[\d+\](?!\w))/g, "");
+
+    // remove "Citations:" section outside of code blocks
     const citationIndex = newText.indexOf("Citations:");
     if (citationIndex !== -1) {
       newText = newText.substring(0, citationIndex);
     }
 
+    // remove bracketed citations inside of code blocks
+    newText = newText.replace(/`[^`]*`|\n```[\s\S]*?\n```/g, (match) => {
+      if (match.startsWith("`")) {
+        return match;
+      } else {
+        return match.replace(/(?<!`)((?<!\w)\[\d+\](?!\w))/g, "");
+      }
+    });
+
     return newText;
   };
-
   return (
     <div className="container">
-      <h1 className="title">Remove Citations from Perplexity Output</h1>
-      <button className="button" onClick={handlePaste}>
-        Paste from Clipboard
-      </button>
-
+      <h2 className="title">Remove Citations from Perplexity Output</h2>
+      <div className="button-container">
+        <button className="button" onClick={handlePaste}>
+          <FaPaste className="icon" />
+          Paste
+        </button>
+        <button className="button" onClick={handleCopy} disabled={!text.trim()}>
+          <FaCopy className="icon" />
+          Copy
+        </button>
+        <button className="button" onClick={handleClear}>
+          <FaTrash className="icon" />
+        </button>
+      </div>
       <textarea
         className="textarea"
         placeholder="Paste text here"
         value={text}
         onChange={(e) => setText(removeCitations(e.target.value))}
       />
+      {showToast && <div className="toast">Copied!</div>}
     </div>
   );
 };
